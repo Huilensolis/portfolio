83d68978a7c98b173591cd8fdcade27a6da1179c
Huilen Solis <xywvanz@gmail.com>
2023-07-03 23:08:14 -0300

    implemented emailjs service

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { EmailJsService } from 'src/app/services/email-js.service';
+
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+    constructor(private emailJsService: EmailJsService){
+
+    }
+    sendEmail(e: Event){
+        e.preventDefault();
+
+        this.emailJsService.sendEmail(e)
+    }
+
 }
