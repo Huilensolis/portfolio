8cf77c7ad480cbe08553e1d15ccb9eded6d46862
Huilen Solis <xywvanz@gmail.com>
2023-07-05 00:57:20 -0300

    feature: header is responsive

--- src/app/components/top-header/top-header.component.ts
+++ src/app/components/top-header/top-header.component.ts
@@ -1,43 +1,51 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NavItem } from '../../interfaces/nav.models'
+import { NavItem } from '../../interfaces/nav.models';
 import { NavService } from 'src/app/services/nav.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-top-header',
-  templateUrl: './top-header.component.html',
-  styleUrls: ['./top-header.component.scss']
+    selector: 'app-top-header',
+    templateUrl: './top-header.component.html',
+    styleUrls: ['./top-header.component.scss'],
 })
 export class TopHeaderComponent {
     navItems: NavItem[] = [];
     actualSectionInNav: string = '';
 
     private navItemsSubsctiption!: Subscription;
-    constructor(private navService: NavService){
+    constructor(private navService: NavService) {}
+
+    // toggle menu
+    menuActive: boolean = false;
+    toggleMenuState(){
+        this.menuActive = !this.menuActive;
+    }
+
+    ngOnInit(): void {
+        this.navItems = this.navService.getNavItems();
+
+        this.navItemsSubsctiption = this.navService
+            .getSubsctiption()
+            .subscribe(() => {
+                this.updateItems();
+            });
 
     }
 
-    ngOnInit(): void{
-        this.navItems = this.navService.getNavItems()
 
-        this.navItemsSubsctiption = this.navService.getSubsctiption().subscribe(() => {
-            this.updateItems()
-        })
+    private updateItems() {
+        this.navItems = this.navService.getNavItems();
+
+        let ActiveItem = this.navItems.find((item) => item.isActive === true);
+        if (!ActiveItem) return;
+        this.actualSectionInNav = ActiveItem?.title;
     }
 
-    private updateItems(){
-        this.navItems = this.navService.getNavItems()
-
-        let ActiveItem = this.navItems.find(item => item.isActive === true)
-        if(!ActiveItem) return
-        this.actualSectionInNav = ActiveItem?.title
+    SetNavItemsState(title: any) {
+        this.navService.setActive(title);
     }
 
-    SetNavItemsState(title: any){
-        this.navService.setActive(title)
-    }
-
-    ngOnDestroy(): void{
+    ngOnDestroy(): void {
         this.navItemsSubsctiption.unsubscribe();
     }
 }
