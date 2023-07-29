0d07e5cc9153abafe5a5249d521a0fd1bb23e4a5
Huilen Solis <xywvanz@gmail.com>
2023-07-29 19:08:45 -0300

    updated tiles

--- src/app/components/projects-section/projects-section.component.ts
+++ src/app/components/projects-section/projects-section.component.ts
@@ -20,7 +20,7 @@ export class ProjectsSectionComponent {
                 'A new way to share recipes online, letting user log in, sign in while posting, deleting, sharing, liking, updating recipes',
             githubLink: 'https://github.com/CulinaryAlchemy/CulinaryAlchemy',
             responsabilities:
-                'As a Fullstack Developer in this project, I was responsible for building the backend database, managing API routes, implementing API security, and handling authorizations',
+                'As a Backend Developer on a 2 people team, I was responsible for building the backend database, managing API routes, implementing API security, and handling authorizations',
             technologies: ['react', 'node', 'express', 'sequelize'],
         },
         {
