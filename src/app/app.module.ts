bc99c41cde59d7fa5ed151ed6377fdf732f1e02e
Huilen Solis <xywvanz@gmail.com>
2023-09-03 00:54:17 -0300

    removed skillset component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { TopHeaderComponent } from './components/top-header/top-header.component';
 import { TitleComponent } from './components/title/title.component';
 import { AboutComponent } from './components/about/about.component';
-import { SkillSetComponent } from './components/skill-set/skill-set.component';
 import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
 import { ProjectTemplateComponent } from './components/project-template/project-template.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
@@ -20,7 +19,6 @@ import { TechnologieComponent } from './components/technologies/technologies.com
     TopHeaderComponent,
     TitleComponent,
     AboutComponent,
-    SkillSetComponent,
     ProjectsSectionComponent,
     ProjectTemplateComponent,
     ContactFormComponent,
