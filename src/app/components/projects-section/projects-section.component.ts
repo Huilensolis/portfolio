c040337e1d6a2b78c56ae3bcb8f99ab3291149b0
Huilen Solis <xywvanz@gmail.com>
2023-09-03 00:45:41 -0300

    implemented new design

--- src/app/components/projects-section/projects-section.component.ts
+++ src/app/components/projects-section/projects-section.component.ts
@@ -17,24 +17,24 @@ export class ProjectsSectionComponent {
         {
             title: 'Culinary Alchemy',
             description:
-                'A new way to share recipes online, letting users log in, sign in while posting, deleting, sharing, liking, updating recipes',
+                'A platform that aims to revolutionize the way people discover, share, and explore recipes.',
             githubLink: 'https://github.com/CulinaryAlchemy/CulinaryAlchemy',
             deployLink: 'https://culinary-alchemy-web-app.vercel.app/',
             responsabilities:
-                'As a Backend Developer on a 2 people team, I was responsible for building the database , its services and controllers, implementing API routes, API security, authentications and authorizations',
+                'As a Backend Developer on a 2 people team, I was responsible for building the database, its services and controllers, implementing API routes, API security, authentications and authorizations',
             technologies: ['react', 'node', 'express', 'sequelize'],
         },
         {
             title: 'Spotify landing page clone',
-            description:
-                'Vanilla project to clone the Spotify landing page',
+            description: 'Vanilla project to clone the Spotify landing page',
             githubLink: 'https://github.com/Huilensolis/Spotify-clone',
             deployLink: 'https://spotify-clone-vanilla.vercel.app/',
             technologies: ['html', 'css'],
         },
         {
             title: 'Knight memory card game',
-            description: 'Classic memory card game, where you must memorize all cards to match them, while running a cronometro to track your mark!',
+            description:
+                'Classic memory card game, where you must memorize all cards to match them, while running a cronometro to track your mark!',
             githubLink: 'https://github.com/Huilensolis/Memory-cards-game',
             deployLink: 'https://medieval-memory-card-game.vercel.app/',
             technologies: ['html', 'css', 'javascript'],
